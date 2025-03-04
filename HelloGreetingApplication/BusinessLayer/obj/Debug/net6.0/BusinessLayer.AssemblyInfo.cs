bb54e28f32757b65cf6329a06af55927d22e7ae9
Rohit <rohitdixit570@gmail.com>
2025-03-04 10:25:34 +0530

    [Rohit] Merge: UC1 branch merged into main

--- HelloGreetingApplication/BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
+++ HelloGreetingApplication/BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4f408b5202674ddac021a6deca3a6ed2d858f28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f37166cd41bdbde4b82c1875d9ed0af068581aed")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
