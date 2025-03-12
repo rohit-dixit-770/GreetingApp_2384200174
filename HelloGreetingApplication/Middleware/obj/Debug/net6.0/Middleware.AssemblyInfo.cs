c763c7f4ac9692e53e801eace2fbfce7af5d69eb
Rohit <rohitdixit570@gmail.com>
2025-03-13 00:30:25 +0530

    [Rohit] Merge: UC10 merged

--- HelloGreetingApplication/Middleware/obj/Debug/net6.0/Middleware.AssemblyInfo.cs
+++ HelloGreetingApplication/Middleware/obj/Debug/net6.0/Middleware.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Middleware")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f2e61f1f0913aad2376632b600a38edcdfd66ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2828702498ba47714d4bfedafc12334146a2f3e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Middleware")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Middleware")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
